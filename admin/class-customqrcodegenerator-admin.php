7b94002465a81d400c6bc1d833466ffd8747261b
bhavin1998 <bhavingediya123@gmail.com>
2022-06-17 13:12:38 +0530

    QR code js is added

--- admin/class-customqrcodegenerator-admin.php
+++ admin/class-customqrcodegenerator-admin.php
@@ -96,7 +96,9 @@ class Customqrcodegenerator_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/customqrcodegenerator-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( 'main-jquery-js', plugin_dir_url( __FILE__ ) . 'js/jquery-min.js', array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( 'jquery-custom-qr', plugin_dir_url( __FILE__ ) . 'js/jqueryqrcode.js', array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/customqrcodegenerator-admin.js', array( 'jquery' ), $this->version, true );
 
 	}
 
--- public/class-customqrcodegenerator-public.php
+++ public/class-customqrcodegenerator-public.php
@@ -95,9 +95,9 @@ class Customqrcodegenerator_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/customqrcodegenerator-public.js', array( 'jquery' ), $this->version, false );
-
+		wp_enqueue_script( 'main-jquery-js', plugin_dir_url( __FILE__ ) . 'js/jquery-min.js', array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( 'jquery-custom-qr', plugin_dir_url( __FILE__ ) . 'js/jqueryqrcode.js', array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/customqrcodegenerator-public.js', array( 'jquery' ), $this->version, true );
 	}
 
 }
